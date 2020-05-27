625602f1508f430c9f4c5768e5f7d5a581ad93d5
Sam Chung <tieoneease@gmail.com>
2020-05-27 14:00:14 -0400

    ag

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
+#install ag
+apt-get install silversearcher-ag
+
 # install zsh on ubuntu
 sudo apt-get update && sudo apt-get -y install zsh
 
