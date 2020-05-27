cd6f688c770f0617c4b212786ec24b670c3306c6
Sam Chung <tieoneease@gmail.com>
2020-05-27 12:55:48 -0400

    init

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash 
+
+# install zsh on ubuntu
+sudo apt-get update && sudo apt-get -y install zsh
+
+# install zsh on mac
+brew install zsh
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# symlink zshrc
+ln -s ~/.config/dotfiles/zshrc ~/.zshrc
