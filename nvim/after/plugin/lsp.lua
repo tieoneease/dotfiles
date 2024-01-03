4e1c8c226f10e493e934bd9a447e41a45e82fc86
Sam <tieoneease@gmail.com>
2024-01-03 08:58:10 -0800

    return also confirms lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -15,6 +15,7 @@ cmp.setup({
   },
   mapping = cmp.mapping.preset.insert({
     ['<Tab>'] = cmp.mapping.confirm({select = true}),
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
     ['<C-Space>'] = cmp.mapping.complete(),
   }),
 })
