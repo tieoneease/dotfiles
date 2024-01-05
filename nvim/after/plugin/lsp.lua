908e029f1b037c01dc86674f2d79e2524ec25850
Sam Chung <sam.chung@peachystudio.com>
2024-01-05 02:19:07 -0800

    lsp config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,25 +1,5 @@
 local lsp = require('lsp-zero')
 
-lsp.preset('recommended')
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-
-cmp.setup({
-  sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp'},
-    {name = 'nvim_lua'},
-    {name = 'luasnip', keyword_length = 2},
-    {name = 'buffer', keyword_length = 3},
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<Tab>'] = cmp.mapping.confirm({select = false}),
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }),
-})
-
 lsp.on_attach(function(client, bufnr)
     local opts = {buffer = bufnr, remap = false}
     vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
@@ -40,20 +20,33 @@ lsp.on_attach(function(client, bufnr)
     end, opts)
 end)
 
-lsp.setup()
-
-vim.diagnostic.config({
-    virtual_text = true
-})
-
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = {
-        'tsserver',
-        'rust_analyzer',
-        'svelte',
-    },
-    handlers = {
-        lsp.default_setup,
-    }
+  ensure_installed = {'tsserver', 'rust_analyzer', 'svelte'},
+  handlers = {
+    lsp.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+  }
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
+cmp.setup({
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp'},
+    {name = 'nvim_lua'},
+    {name = 'luasnip', keyword_length = 2},
+    {name = 'buffer', keyword_length = 3},
+  },
+  formatting = lsp.cmp_format(),
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = cmp.mapping.confirm({select = false}),
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }),
 })
