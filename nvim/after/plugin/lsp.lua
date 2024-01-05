9882ee99df85b67709004131570105b4a10f6728
Sam Chung <sam.chung@peachystudio.com>
2024-01-05 11:35:49 -0800

    add snip completion

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -35,6 +35,12 @@ require('mason-lspconfig').setup({
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
+local luasnip = require('luasnip')
+local has_words_before = function()
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  return (vim.api.nvim_buf_get_lines(0, cursor[1] - 1, cursor[1], true)[1] or ''):sub(cursor[2], cursor[2]):match('%s') 
+end
+
 cmp.setup({
     sources = {
         {name = 'path'},
