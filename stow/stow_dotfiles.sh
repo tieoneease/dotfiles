f433f9c5bc8bfc7f0ab046fb7736187e6619a81a
Sam Chung <tieoneease@gmail.com>
2025-03-28 17:21:50 -0700

    tms config

--- stow/stow_dotfiles.sh
+++ stow/stow_dotfiles.sh
@@ -29,7 +29,7 @@ backup_existing_files() {
     fi
     
     # Check for other common config directories that might exist
-    for dir in nvim kitty hypr waybar; do
+    for dir in nvim kitty hypr waybar tms; do
         if [ -d "${TARGET_DIR}/$dir" ] && [ ! -L "${TARGET_DIR}/$dir" ]; then
             echo "Backing up $dir configuration..."
             cp -r "${TARGET_DIR}/$dir" "$BACKUP_DIR/"
