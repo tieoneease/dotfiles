d519daf6b2914b55660695645055d7f8e799e5c0
Sam Chung <tieoneease@gmail.com>
2025-04-03 12:44:36 -0700

    add glow

--- arch_setup.sh
+++ arch_setup.sh
@@ -58,6 +58,10 @@ echo "Installing Hyprland and related packages..."
 install_packages hyprland waybar wofi wl-clipboard xdg-desktop-portal-hyprland \
     qt5-wayland qt6-wayland polkit-kde-agent grim slurp swappy hyprpaper brightnessctl grimblast
 
+# Install tools
+echo "Installing Hyprland and related packages..."
+install_packages glow
+
 # Install terminal emulator
 echo "Installing kitty terminal..."
 install_packages kitty
