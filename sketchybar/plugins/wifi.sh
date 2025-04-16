97dd6e28eb32be207c8fbf7690373dfd49748956
Sam Chung <tieoneease@gmail.com>
2025-04-15 21:33:35 -0700

    fix wifi on mac

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -9,11 +9,24 @@ if [ "$WIFI_POWER" = "Off" ]; then
     exit 0
 fi
 
-# Get current Wi-Fi connection info
-CURRENT_WIFI=$(networksetup -getairportnetwork $WIFI_DEVICE | awk -F": " '{print $2}')
+# Check Wi-Fi connection using more reliable methods
+# Try multiple methods to determine if Wi-Fi is connected
 
-if [ "$CURRENT_WIFI" = "off" ] || [ -z "$CURRENT_WIFI" ]; then
-    sketchybar --set wifi icon="󰖪" label=""
-else
+# Method 1: Use ifconfig to check if interface has an IP
+WIFI_IP=$(ifconfig $WIFI_DEVICE 2>/dev/null | grep "inet " | awk '{print $2}')
+
+# Method 2: Try airport command if available
+if command -v airport &> /dev/null; then
+    AIRPORT_INFO=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)
+    SSID=$(echo "$AIRPORT_INFO" | awk -F': ' '/ SSID/ {print $2}')
+elif [ -f "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport" ]; then
+    AIRPORT_INFO=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)
+    SSID=$(echo "$AIRPORT_INFO" | awk -F': ' '/ SSID/ {print $2}')
+fi
+
+# Check if we have either an IP address or an SSID
+if [ -n "$WIFI_IP" ] || [ -n "$SSID" ]; then
     sketchybar --set wifi icon="󰖩" label=""
+else
+    sketchybar --set wifi icon="󰖪" label=""
 fi
